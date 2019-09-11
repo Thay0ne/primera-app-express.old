b97768a264ff850cebbbbfd70aa5ce3df7ee0d7d
Gina <ginna_44@hotmail.com>
2019-09-11 16:14:10 -0500

    nuevo query string 3

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/:nombre', (req, res) => {
-  res.send('<h1>Hola </h1>' + req.params.nombre );
+app.get('/', (req, res) => {
+  res.send('<h1>Hola </h1>' + req.query.nombre );
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
