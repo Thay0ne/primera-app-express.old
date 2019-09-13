ecc11f0da77ba4e9ae3021226ccabe033251e708
Gina <ginna_44@hotmail.com>
2019-09-12 19:57:08 -0500

    Se elimina libreria express-useragent y se usa useragent directo

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-const useragent = require('express-useragent');
 
-app.use(useragent.express());
 app.get('/', function(req, res){
-  res.send(req.useragent);
+    res.send(req.get('User-Agent'));
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
