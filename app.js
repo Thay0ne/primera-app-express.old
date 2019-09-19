ad15795abfddaacdea35c54ec86fc772fbf6dc35
Gina <ginna_44@hotmail.com>
2019-09-18 22:46:55 -0500

    Whole new  visitantes recurrentes

--- app.js
+++ app.js
@@ -10,33 +10,107 @@ var schema = mongoose.Schema({
 
 var Visitor = mongoose.model("Visitor", schema);
 
+function tablaHtml(datos) {
+  var tabla = '<table>'+
+  '<thead>'+
+  '<tr>' +
+  '<th>_id</th>' +
+  '<th>name</th>' +
+  '<th>count</th>' +
+  '</tr>' +
+  '</thead>'+
+  '<tbody>';
+  for (var i = 0; i < datos.length; i++) {
+    tabla += '<tr>'+
+    '<td>'+datos[i]._id+'</td>' +
+    '<td>'+datos[i].name+'</td>' +
+    '<td>'+datos[i].count+'</td>' +
+    '</tr>';
+  }
+  tabla += '</tbody>' + '</table>';
+  return tabla;
+}
 app.get('/', function(req, res){
+
   if (req.query.name) {
-    Visitor.find({ name: req.query.name }, function(err, visitante) {
-  if (err) return console.error(err);
-  if(visitante){
-    Visitor.update({ name: req.query.name }, function(err) {
-  if (err) return console.error(err);
-});
-  }else{
-    Visitor.create({ name: req.query.name }, function(err) {
+    Visitor.findOne({ name: req.query.name }, function(err, visitante) {
       if (err) return console.error(err);
+      if (visitante){
+        visitante.count += 1;
+
+        visitante.save(function(err) {
+          if (err) return console.error(err);
+          Visitor.find(function(err, visitantes) {
+            if (err) return console.error(err);
+            res.send(tablaHtml(visitantes));
+          });
+        });
+      }
+      else {
+        Visitor.create({ name: req.query.name }, function(err) {
+          if (err) return console.error(err);
+          Visitor.find(function(err, visitantes) {
+            if (err) return console.error(err);
+            res.send(tablaHtml(visitantes));
+          });
+        });
+      }
+
+    });
+
+  } else {
+    Visitor.create({ name: "Anónimo" }, function(err) {
+      if (err) return console.error(err);
+      Visitor.find(function(err, visitantes) {
+        if (err) return console.error(err);
+        res.send(tablaHtml(visitantes));
+      });
     });
   }
+
+});
+app.listen(3000, () => console.log('Listening on port 3000!'));
+
+
+/*const express = require('express');
+const app = express();
+var mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true, useUnifiedTopology: true });
+
+var schema = mongoose.Schema({
+name: {type: String, default: "Anónimo" },
+count: {type: Number, default: 1 }
 });
 
-  }
+var Visitor = mongoose.model("Visitor", schema);
+
+app.get('/', function(req, res){
+if (req.query.name) {
+Visitor.findOne({ name: req.query.name }, function(err, visitante) {
+if (err) return console.error(err);
+if(visitante){
+Visitor.update({ count: req.query.name }, function(err) {
+if (err) return console.error(err);
+});
+}else{
+Visitor.create({ name: "Anónimo", count: 1 }, function(err) {
+if (err) return console.error(err);
+});
+}
+});
+
+}
 res.send(
-    '<table style="width:100%">'+
-    '<thead>'+
-    '<tr>' +
-    '<th>_id</th>' +
-    '<th>name</th>' +
-    '<th>count</th>' +
-    '</tr>' +
-    '</thead>'+
-    '</table>' + req.query.name
-   );
+'<table style="width:100%">'+
+'<thead>'+
+'<tr>' +
+'<th>_id</th>' +
+'<th>name</th>' +
+'<th>count</th>' +
+'</tr>' +
+'</thead>'+
+'</table>' + req.query.name
+);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
@@ -48,18 +122,18 @@ var mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true, useUnifiedTopology: true });
 
 var schema = mongoose.Schema({
-  date: {type: Date, default: Date.now()},
-  name: {type: String, default: "Anónimo" }
+date: {type: Date, default: Date.now()},
+name: {type: String, default: "Anónimo" }
 
 });
 
 var Visitor = mongoose.model("Visitor", schema);
 
 app.get('/', function(req, res){
-  Visitor.create({ name: req.query.name }, function(err) {
-    if (err) return console.error(err);
-  });
-  res.send('<h1>"El visitante fue almacenado con éxito"</h1>' + req.query.name);
+Visitor.create({ name: req.query.name }, function(err) {
+if (err) return console.error(err);
+});
+res.send('<h1>"El visitante fue almacenado con éxito"</h1>' + req.query.name);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
@@ -75,33 +149,33 @@ mongoose.connection.on("error", function(e) { console.error(e); });
 
 // definimos el schema
 var schema = mongoose.Schema({
-  title: String,
-  body: String,
-  published: { type: Boolean, default: false }
+title: String,
+body: String,
+published: { type: Boolean, default: false }
 });
 
 // definimos el modelo
 var Article = mongoose.model("Article", schema);
 
 Article.create({ title: "Artículo 2", body: "Cuerpo del artículo" }, function(err) {
-  if (err) return console.error(err);
+if (err) return console.error(err);
 });
 
 Article.insertMany([
-  { title: "Artículo 3", body: "Cuerpo del artículo" },
-  { title: "Artículo 4", body: "Cuerpo del artículo" }
+{ title: "Artículo 3", body: "Cuerpo del artículo" },
+{ title: "Artículo 4", body: "Cuerpo del artículo" }
 ], function(err) {
-  if (err) return console.error(err);
+if (err) return console.error(err);
 });
 
 Article.find(function(err, articles) {
-  if (err) return console.error(err);
-  console.log(articles);
+if (err) return console.error(err);
+console.log(articles);
 });
 
 Article.find({ article: "El título" }, function(err, articles) {
-  if (err) return console.error(err);
-  console.log(articles);
+if (err) return console.error(err);
+console.log(articles);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
@@ -111,7 +185,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send(req.get('User-Agent'));
+res.send(req.get('User-Agent'));
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
@@ -123,17 +197,17 @@ const app = express();
 app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
-  res.send(
-  '<form action="/myapp" method="post">' +
-  '<label for="name">'+
-  '<input type="text" id="name" name="name">' +
-  '<button type="submit">Enviar</button>' +
-  '</form>'
-  );
+res.send(
+'<form action="/myapp" method="post">' +
+'<label for="name">'+
+'<input type="text" id="name" name="name">' +
+'<button type="submit">Enviar</button>' +
+'</form>'
+);
 });
 
 app.post('/myapp', (req, res) => {
-  res.send('<h1>Hola ' + req.body.name + '!</h1>');
+res.send('<h1>Hola ' + req.body.name + '!</h1>');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
@@ -144,10 +218,10 @@ const app = express();
 
 app.get('/makers/:name', (req, res) => {
 
-  function capitalizeFirstLetter(name) {
-  return name.charAt(0).toUpperCase() + name.slice(1);
+function capitalizeFirstLetter(name) {
+return name.charAt(0).toUpperCase() + name.slice(1);
 }
-  res.send('<h1>Hola ' + capitalizeFirstLetter(req.params.name) + '!</h1>');
+res.send('<h1>Hola ' + capitalizeFirstLetter(req.params.name) + '!</h1>');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
@@ -157,11 +231,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  var nombre = req.query.nombre;
-  if (!nombre) {
-    nombre = "desconocido";
-  }
-  res.send('<h1>Hola ' + nombre + '!</h1>');
+var nombre = req.query.nombre;
+if (!nombre) {
+nombre = "desconocido";
+}
+res.send('<h1>Hola ' + nombre + '!</h1>');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
